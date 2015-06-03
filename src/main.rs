bae8bceab297334fa7c6b72500c9d5d172d7f204
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-06-03 01:04:06 -0400

    almost done w reading stdin

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 fn main() {
 	let reader = std::io::stdin();
-	let lineReader = reader.lines();
+	let mut lines : Vec<String> = Vec::new();
+	let mut bytes : Vec<&u8> = Vec::new();
+	reader.read_to_end(bytes);
+	match reader.read_to_end(bytes) {
+		Err(e) => println!("{}", e),
+		_ => {}
+	}
 
-	let mut lines : Vec<&str> = vec![];
-	for lineContainer in lineReader.iter() {
-		match lineContainer {
-			Some(Ok(line)) => {
-				lines.push(line);
+	// assume utf8
+	for bytes_line in bytes.iter() {
+		match std::string::String::from_utf8(bytes_line) {
+			Ok(s) => {
+				lines.push(s)
 			},
 			_ => {
 
