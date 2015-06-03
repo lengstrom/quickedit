4c4be38b62a08687e87c5783956900252d9de6aa
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-06-03 00:41:36 -0400

    added ncurses

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 fn main() {
 	let reader = std::io::stdin();
-	let lineReader = reader.lines()
+	let lineReader = reader.lines();
 
-	
-	for i in lineReader.iter() {
-		lines.push(i)
+	let mut lines : Vec<&str> = vec![];
+	for line in lineReader.iter() {
+		match lineContainer {
+			Some(Ok(a)) => {
+				lines.push(line);
+			},
+			_ => {
+
+			}
+		}
+	}
+
+	for i in lines.iter() {
+		println!("{}", i);
 	}
 
 
